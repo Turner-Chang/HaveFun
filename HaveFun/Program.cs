c3feee25e199e9130c39b82052e0b6b7deda7828
Turner <ddwa2007@gmail.com>
2024-06-01 17:30:45 +0800

    新增 Swashbuckle.AspNetCore

--- HaveFun/Program.cs
+++ HaveFun/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var connHaveFunStr = builder.Configuration.GetConnectionString("HaveFunDbContext");
 builder.Services.AddSqlServer<HaveFunDbContext>(connHaveFunStr);
@@ -16,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
