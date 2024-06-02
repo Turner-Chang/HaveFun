a8b6f5f63118a3125a9df4b9ab810e28a6bb5b34
T14 Gen 3 <T14 Gen 3@LAPTOP-EGI1T3KO>
2024-06-02 09:25:06 +0800

    修正swagger在program的啟動時機

--- HaveFun/Program.cs
+++ HaveFun/Program.cs
@@ -14,15 +14,15 @@ builder.Services.AddSqlServer<HaveFunDbContext>(connHaveFunStr);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
