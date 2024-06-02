7ab3ff15bd456305d891f80122f353418539e5e8
Weee <ang498751@gmail.com>
2024-06-02 23:22:42 +0800

    add MemberProfileController(個人資料控制器) 新增畫面 簡單排版 還沒弄出內容

---
+++ HaveFun/Controllers/MemberProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Controllers
+{
+	public class MemberProfileController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
