205a854c39203a603b619bc6fd3271b0a5d1e1c0
Winny <winny14752@gmail.com>
2024-06-03 22:51:27 +0800

    個人貼文頁面v1 & add PostController

---
+++ HaveFun/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
