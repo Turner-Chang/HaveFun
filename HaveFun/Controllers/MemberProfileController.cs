60470617dfcc870e9fb1478c5a462d6ae7da1e2f
Weee <ang498751@gmail.com>
2024-06-03 13:58:41 +0800

    個人資料 頁面排版 add model:MemberProfile(會員資料)

--- HaveFun/Controllers/MemberProfileController.cs
+++ HaveFun/Controllers/MemberProfileController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HaveFun.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace HaveFun.Controllers
 {
 	public class MemberProfileController : Controller
 	{
+		// GET: 
 		public IActionResult Index()
 		{
 			return View();
 		}
 	}
+	
+	
+
+	// POST: Profile/Edit
+	//[HttpPost]
+	//public ActionResult Edit(MemberProfile profile)
+	//{
+		
+	//	// 更新資料庫中的資料
+	//}
 }
---
+++ HaveFun/Models/MemberProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HaveFun.Models
+{
+	public class MemberProfile
+	{
+			[Key]
+			[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+			public int MemberProfileId { get; set; }
+
+			[Required]
+			[MaxLength(10)]
+			public string Nickname { get; set; }
+
+			[Required]
+			[MaxLength(10)]
+			public string Location { get; set; }
+
+			[Required]
+			[Column("BirthDay", TypeName = "Date")]	
+			public DateTime Birthday { get; set; }
+
+			[MaxLength(10)]
+			public string Occupation { get; set; }
+			[MaxLength(30)]
+			public string Interests { get; set; }
+
+			[MaxLength(50)]
+			public string Introduction { get; set; }
+		
+
+	}
+}
