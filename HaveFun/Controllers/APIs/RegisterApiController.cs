bca5d0343c300fd3de5ea19f63e40d4a44a18672
Jerry <inqoas@gmail.com>
2024-06-04 11:06:28 +0800

    Fix UserRegistrationDTO Add RegisterApiController

---
+++ HaveFun/Controllers/APIs/RegisterApiController.cs
@@ -0,0 +1,26 @@
+﻿using HaveFun.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Controllers.APIs
+{
+    [Route("api/Register/[action]")]
+    [ApiController]
+    public class RegisterApiController : ControllerBase
+    {
+        HaveFunDbContext _dbContext;
+
+        public RegisterApiController(HaveFunDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // 驗證帳號是否重複
+        [HttpGet("{account}")]
+        public bool GetHasAccount(string account)
+        {
+            _dbContext
+            return true;
+        }
+    }
+}
