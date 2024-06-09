6e080743f8758eef3a57f151891addfacf1747a7
Jerry <inqoas@gmail.com>
2024-06-09 20:02:50 +0800

    Update RegisterController Add VerificationFailed.cshtml

--- HaveFun/Controllers/RegisterController.cs
+++ HaveFun/Controllers/RegisterController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HaveFun.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HaveFun.Controllers
 {
 	public class RegisterController : Controller
 	{
+		HaveFunDbContext _dbContext;
+		public RegisterController(HaveFunDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
         // 註冊頁面
         // Index: Register
         public IActionResult Index()
@@ -18,11 +26,30 @@ namespace HaveFun.Controllers
 			return View();
 		}
 
-        // Register/Verification
-        // 驗證信箱完成頁面
-        public IActionResult Verification()
+		// Register/Verification/id
+		// 驗證信箱完成頁面
+		[HttpGet("{id}")]
+        public async Task<IActionResult> Verification(int id)
 		{
-			return View();
+			try
+			{
+				UserInfo? user = await _dbContext.UserInfos.FindAsync(id);
+				if (user != null)
+				{
+					user.AccountStatus = 1;
+					await _dbContext.SaveChangesAsync();
+                    return View("VerificationSuccess");
+                }else
+				{
+                    return View("VerificationFailed");
+                }
+				
+			}
+			catch (Exception)
+			{
+
+                return View("VerificationFailed");
+            }
 		}
 	}
 }
