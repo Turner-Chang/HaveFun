8f0e84361a7921c5d7137bc1444df84dc83fed71
Turner <ddwa2007@gmail.com>
2024-06-12 21:21:52 +0800

    1remove friend table from migration and move class to DTO (temp)

--- HaveFun/Migrations/20240612123327_AddUserInfoColumnPost.cs
+++ HaveFun/Migrations/20240612123327_AddUserInfoColumnPost.cs
@@ -14,43 +14,6 @@ namespace HaveFun.Migrations
                 name: "FK_Posts_UserInfos_UserId",
                 table: "Posts");
 
-            migrationBuilder.CreateTable(
-                name: "Friend",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    IsBlocked = table.Column<bool>(type: "bit", nullable: false),
-                    FriendListId = table.Column<int>(type: "int", nullable: true),
-                    FriendListId1 = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Friend", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Friend_FriendLists_FriendListId",
-                        column: x => x.FriendListId,
-                        principalTable: "FriendLists",
-                        principalColumn: "Id");
-                    table.ForeignKey(
-                        name: "FK_Friend_FriendLists_FriendListId1",
-                        column: x => x.FriendListId1,
-                        principalTable: "FriendLists",
-                        principalColumn: "Id");
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Friend_FriendListId",
-                table: "Friend",
-                column: "FriendListId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Friend_FriendListId1",
-                table: "Friend",
-                column: "FriendListId1");
-
             migrationBuilder.AddForeignKey(
                 name: "FK_Posts_UserInfos_UserId",
                 table: "Posts",
