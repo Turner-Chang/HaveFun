d98c1dc0663bb78b05e1a34514f35ccbbc777324
Winny <winny14752@gmail.com>
2024-06-12 19:15:47 +0800

    sqush commit

--- HaveFun/DTOs/PostReviewDTO.cs
+++ HaveFun/DTOs/PostReviewDTO.cs
@@ -6,16 +6,16 @@ namespace HaveFun.DTOs
 {
     public class PostReviewDTO
     {
-        public int PostReviewId { get; set; }
+		public int PostReviewId { get; set; }
 		public int UserId { get; set; }
 
 		public int PostId { get; set; }
 		public int ReportItems { get; set; }
 		public string Reason { get; set; }
-
-		public DateTime ReportTime { get; set; }
-
 		public int ProcessingStstus { get; set; } = 0;
 
-    }
+
+
+
+	}
 }
