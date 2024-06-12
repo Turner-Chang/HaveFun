52415f8749a5e3b70e8f0aa8b5ca6b3d684be14f
Jerry <inqoas@gmail.com>
2024-06-12 09:34:21 +0800

    Add UserLoginDTO LoginApiController Update Login/Index.cshtml

---
+++ HaveFun/Controllers/APIs/LoginApiController.cs
@@ -0,0 +1,18 @@
+﻿using HaveFun.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Controllers.APIs
+{
+    [Route("api/login/[action]")]
+    [ApiController]
+    public class LoginApiController : ControllerBase
+    {
+        public async Task<JsonResult> Login(UserLoginDTO user)
+        {
+            if (!ModelState.IsValid)
+            {
+            }
+        }
+    }
+}
