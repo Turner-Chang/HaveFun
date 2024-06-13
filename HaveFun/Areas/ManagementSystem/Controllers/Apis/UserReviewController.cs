158d4c6ebdf5a7a09ff789106f4bf798627c093c
Sam <whps60506@gmail.com>
2024-06-13 17:56:53 +0800

    add userPreview get set in DBcontext  , preview table

---
+++ HaveFun/Areas/ManagementSystem/Controllers/Apis/UserReviewController.cs
@@ -0,0 +1,34 @@
+﻿using HaveFun.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HaveFun.Areas.ManagementSystem.Controllers.Apis
+{
+    [Route("api/userReview/[action]")]
+    [ApiController]
+    public class UserReviewController : ControllerBase
+    {
+        private HaveFunDbContext _context;
+        public UserReviewController(HaveFunDbContext haveFunDbContext)
+        {
+            _context = haveFunDbContext;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<UserReview>> GetAll()
+        {
+            return _context.UserReviews;
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserReview>> GetById(int id) { 
+        var review1 = await _context.UserReviews.FindAsync(id);
+        if(review1 == null)
+            {
+                return NotFound();
+            }
+        return review1;
+        }
+    }
+}
