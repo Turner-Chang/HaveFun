294421332dff0d1828290387673a4fbb37c00785
iven <iven840320@gmail.com>
2024-06-15 13:25:48 +0800

    Reapply "Merge branch 'main' into tim06-02chat"
    This reverts commit 03ef8890943aaf0b8b385d736d8551bee26eb76d.

--- HaveFun/Areas/ManagementSystem/Controllers/HomeController.cs
+++ HaveFun/Areas/ManagementSystem/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace HaveFun.Areas.ManagementSystem.Controllers
         {
             return View();
         }
-    }
+		public IActionResult UserManagement()
+		{
+			return View();
+		}
+	}
 }
---
+++ HaveFun/Areas/ManagementSystem/DTOs/ChangePostStateDTO.cs
@@ -0,0 +1,16 @@
+﻿using HaveFun.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HaveFun.Areas.ManagementSystem.DTOs
+{
+    public class ChangePostStateDTO
+    {
+        public int Id { get; set; }
+
+
+        public int Status { get; set; }
+
+
+    }
+}
