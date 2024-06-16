d5ec7b815be7d2a8ad1fafd6eff689e335af75f3
Daniel <danielchen0810@gmail.com>
2024-06-16 15:55:52 +0800

    update-database

---
+++ HaveFun/Migrations/20240616075503_add userreview_status.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HaveFun.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduserreview_status : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "UserReviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "UserReviews");
+        }
+    }
+}
