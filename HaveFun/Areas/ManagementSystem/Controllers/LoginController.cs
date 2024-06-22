2cd61c2ff32ab29c13c7f50b59b13749d2b8a0f5
Jerry <inqoas@gmail.com>
2024-06-22 15:19:35 +0800

    ADD

---
+++ HaveFun/Areas/ManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Areas.ManagementSystem.Controllers
+{
+    [Area("ManagementSystem")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HaveFun/Models/ManagemenLogin.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HaveFun.Models
+{
+    public class ManagemenLogin
+    {
+        [Key] // 主鍵
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // 設定自動編號
+        public int Id { get; set; }
+
+        [Required]
+        public string Account { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:20,MinimumLength =8)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
