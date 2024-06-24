e92740774676751e5a3da573222cb8d59c617a71
Daniel <danielchen0810@gmail.com>
2024-06-24 20:53:47 +0800

    設置route，可以從活動頁點擊參與者頭像跳到參與者的個人主頁

--- HaveFun/Controllers/ProfileController.cs
+++ HaveFun/Controllers/ProfileController.cs
@@ -6,36 +6,48 @@ namespace HaveFun.Controllers
     public class ProfileController : Controller
     {
         [Authorize(AuthenticationSchemes = "Bearer,Cookies")]
-        public IActionResult Index()
+		[HttpGet("Profile/{id?}")]
+		public IActionResult Index(int? id)
         {
-            var loginUser = Convert.ToInt32(Request.Cookies["userId"]);
-            ViewBag.UserId = loginUser;
-            return View();
-        }
-        //public IActionResult Post()
-        //{
+			int loginUser;
+			if (id.HasValue && id > 0)
+			{
+				// Use the userId parameter if it's valid
+				loginUser = id.Value;
+			}
+			else
+			{
+				var cookieUserId = Request.Cookies["userId"];
+				loginUser = !string.IsNullOrEmpty(cookieUserId) ? Convert.ToInt32(cookieUserId) : 0;
+			}
+			ViewBag.UserId = loginUser;
+			return View();
+		}
 
-        //	return PartialView("_Post");
-        //}
+		//public IActionResult Post()
+		//{
 
-        //public IActionResult UserInfo()
-        //{
-        //	return PartialView("_UserInfo");
-        //}
+		//	return PartialView("_Post");
+		//}
 
-        //public IActionResult WhoLikes()
-        //{
-        //	return PartialView("_WhoLikes");
-        //}
+		//public IActionResult UserInfo()
+		//{
+		//	return PartialView("_UserInfo");
+		//}
 
-        //      public IActionResult FriendList()
-        //      {
-        //          return PartialView("_FriendList");
-        //      }
+		//public IActionResult WhoLikes()
+		//{
+		//	return PartialView("_WhoLikes");
+		//}
 
-        //public IActionResult ActivitysManagement()
-        //{
-        //	return PartialView("_ActivitysManagement");
-        //}
-    }
+		//      public IActionResult FriendList()
+		//      {
+		//          return PartialView("_FriendList");
+		//      }
+
+		//public IActionResult ActivitysManagement()
+		//{
+		//	return PartialView("_ActivitysManagement");
+		//}
+	}
 }
