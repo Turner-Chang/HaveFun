d95492e1707cef495c2194b40c725219784665cf
Jerry <inqoas@gmail.com>
2024-06-25 00:24:39 +0800

    Add Management login

--- HaveFun/Areas/ManagementSystem/Controllers/LoginController.cs
+++ HaveFun/Areas/ManagementSystem/Controllers/LoginController.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HaveFun.Areas.ManagementSystem.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using HaveFun.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace HaveFun.Areas.ManagementSystem.Controllers
 {
     [Area("ManagementSystem")]
     public class LoginController : Controller
     {
+        HaveFunDbContext _dbContext;
+        public LoginController(HaveFunDbContext haveFunDbContext)
+        {
+            _dbContext = haveFunDbContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Index(ManagementLoginDTO loginDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                ManagemenLogin? user = await _dbContext.ManagemenLogins.FirstOrDefaultAsync(user => user.Account == loginDTO.Account);
+                if(user == null || user.Password != loginDTO.Password)
+                {
+                    ModelState.AddModelError("checkError", "帳號或密碼錯誤");
+                    ViewData["error"] = "帳號密碼錯誤";
+                    return View(loginDTO);
+                }
+                HttpContext.Session.SetString("Login", user.Id.ToString());
+                return RedirectToAction("Index", "Home", new { area = "ManagementSystem" });
+            }
+            return View(loginDTO);
+        }
     }
 }
--- HaveFun/Models/ManagemenLogin.cs
+++ HaveFun/Models/ManagemenLogin.cs
@@ -10,13 +10,13 @@ namespace HaveFun.Models
         public int Id { get; set; }
 
         [Required]
-        public string Account { get; set; }
+        public string? Account { get; set; }
 
         [Required]
         [StringLength(maximumLength:20,MinimumLength =8)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
