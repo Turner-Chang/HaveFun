a611c876149226998b2cac61cbbd9e24b914744b
Christopher <Allen71798900@gmail.com>
2024-06-26 15:44:38 +0800

    新增會員等級前端

---
+++ HaveFun/Controllers/MemberLevelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HaveFun.Controllers
+{
+	public class MemberLevelController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
