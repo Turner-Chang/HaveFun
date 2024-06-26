1bccf89c587414aa236ea3426afc20c2fde9db7f
iven <iven840320@gmail.com>
2024-06-26 13:43:08 +0800

    2

--- HaveFun/ChatHub.cs
+++ HaveFun/ChatHub.cs
@@ -12,40 +12,40 @@ public class ChatHub : Hub
 
     }
 
-    public  async override   Task OnConnectedAsync()
+    public override Task OnConnectedAsync()
     {
         Clients.All.SendAsync("SomeOneOnline", Context.ConnectionId);
-        int userId = 2; // 替換為實際的用戶 ID 獲取邏輯
+        //int userId = 2; // 替換為實際的用戶 ID 獲取邏輯
 
         //// 檢查是否已存在相同的 connId
-        var existingConnIdUserId = _context.ConId_UserId
-           .FirstOrDefault(c => c.connId == Context.ConnectionId);
+        //var existingConnIdUserId = _context.ConId_UserId
+        //    .FirstOrDefault(c => c.connId == Context.ConnectionId);
 
-        if (existingConnIdUserId == null)
-        {
-            var userInfo = _context.UserInfos.Find(userId); // 查找 UserInfo 實體
+        //if (existingConnIdUserId == null)
+        //{
+        //    var userInfo = _context.UserInfos.Find(userId); // 查找 UserInfo 實體
 
-            if (userInfo != null)
-            {
-                var connIdUserId = new ConId_UserId
-                {
-                    Id = userId,
-                    connId = Context.ConnectionId,
-                };
+        //    if (userInfo != null)
+        //    {
+        //        var connIdUserId = new ConId_UserId
+        //        {
+        //            Id = userId,
+        //            connId = Context.ConnectionId,
+        //        };
 
-                // 將新物件添加到 DbContext 中
-                _context.ConId_UserId.Add(connIdUserId);
-            }
-        }
-        else
-        {
-            // 如果存在，則不需要新增
-        }
+        //        // 將新物件添加到 DbContext 中
+        //        _context.ConId_UserId.Add(connIdUserId);
+        //    }
+        //}
+        //else
+        //{
+        //    // 如果存在，則不需要新增
+        //}
 
         //// 保存變更到資料庫
-        await _context.SaveChangesAsync();
+        //await _context.SaveChangesAsync();
 
-        await base.OnConnectedAsync();
+        return base.OnConnectedAsync();
     }
 
 
