dea7a42b1fd0a87d3c2f9fa23e6c060cbc8a511b
iven <iven840320@gmail.com>
2024-06-30 12:28:59 +0800

    no message

--- HaveFun/Program.cs
+++ HaveFun/Program.cs
@@ -12,7 +12,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Logging.AddAzureWebAppDiagnostics();
+//builder.Logging.AddAzureWebAppDiagnostics();
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
