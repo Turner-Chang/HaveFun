06512522d808d2736db4f929c479eb115ccca53f
Turner <ddwa2007@gmail.com>
2024-06-30 12:13:44 +0800

    新增 Log stream 查看應用程式紀錄 安裝 Microsoft.Extensions.Logging.AzureAppServices builder.Logging.AddAzureWebAppDiagnostics();

--- HaveFun/Program.cs
+++ HaveFun/Program.cs
@@ -12,6 +12,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.AddAzureWebAppDiagnostics();
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
